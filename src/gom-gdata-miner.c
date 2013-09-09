9980ed4788e7df0f8e901c9d781e0704e3f3a7f0
Debarshi Ray <debarshir@gnome.org>
2013-09-09 18:11:14 +0200

    gdata: Fix typo
    All the arguments were off by one. Pass NULL for graph because that is
    what we are using for folders.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=694003

--- src/gom-gdata-miner.c
+++ src/gom-gdata-miner.c
@@ -162,7 +162,7 @@ account_miner_job_process_entry (GomAccountMinerJob *job,
 
       parent_resource_urn = gom_tracker_sparql_connection_ensure_resource
         (job->connection, job->cancellable, error,
-         NULL, parent_resource_id,
+         NULL, NULL, parent_resource_id,
          "nfo:RemoteDataObject", "nfo:DataContainer", NULL);
       g_free (parent_resource_id);
 
